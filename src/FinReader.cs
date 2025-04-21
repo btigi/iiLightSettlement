4ae694ad2c298c94899929e9e588a18bcd8c062c
btigi <btigi@users.noreply.github.com>
2025-04-21 13:19:01 +0100

    Initial FIN work

---
+++ src/FinReader.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+public class FinReader
+{
+    public void Process(string fileName)
+    {
+        using (var br = new BinaryReader(File.OpenRead(fileName)))
+        {
+            var signature = br.ReadInt16(); // 0x1d00
+            var count2 = br.ReadInt16();
+            var sprCount = br.ReadInt16();
+            var count4 = br.ReadInt16();
+
+            for (int i = 0; i < sprCount; i++)
+            {
+                var sprFilenameBytes = br.ReadBytes(8);
+                var sprFilename = Encoding.ASCII.GetString(sprFilenameBytes);
+            }
+        }
+    }
+}
