419468ce13554a8a9f6ab3f61506407bd6ab3657
btigi <btigi@users.noreply.github.com>
2025-04-26 10:53:28 +0100

    FIN updates

--- src/FinReader.cs
+++ src/FinReader.cs
@@ -7,12 +7,18 @@ public class FinReader
         using var br = new BinaryReader(File.OpenRead(fileName));
         var signature = br.ReadInt16(); // 0x1d00
         var count2 = br.ReadInt16();
+        var dataBlockCount = br.ReadInt16();
         var sprCount = br.ReadInt16();
-        var count4 = br.ReadInt16();
 
         for (int i = 0; i < sprCount; i++)
         {
             var sprFilenameBytes = br.ReadBytes(8);
+            var sprFilename = Encoding.ASCII.GetString(sprFilenameBytes);
+        }
+
+        for (int i = 0; i < dataBlockCount; i++)
+        {
+            var sprFilenameBytes = br.ReadBytes(20);
             var sprFilename = Encoding.ASCII.GetString(sprFilenameBytes);
         }
     }
